2e37f39dce4243e2f6758492f9146386fa5de0cb
Bart Willems <ps3bartwillems@gmail.com>
2017-01-21 00:09:05 +0100

    Added some fake delay bullshit
    Signed-off-by: Bart Willems <ps3bartwillems@gmail.com>

--- src/js/getYoutubeVideos.js
+++ src/js/getYoutubeVideos.js
@@ -2,14 +2,35 @@ $(function() {
 
     var searchBar       = $('#searchBar');
     var youtubeResult   = $('#youtubeResult');
+    var videos          = youtubeResult.html();
 
     $('#youtubeQuery').keypress(function (e) {
         if (e.which == 13) {
+            $('form#search').submit();
             searchBar.addClass('loading disabled');
-            // This should go in the beforeSend API
             var loadingHTML  = '<div class="ui active inverted dimmer">';
-                loadingHTML += '<div class="ui text loader">Loading</div></div><p></p>';
+            loadingHTML += '<div class="ui text loader">Loading</div></div><p></p>';
             youtubeResult.html(loadingHTML);
+            return false;
         }
     });
+
+    $('form#search')
+        .api({
+            url: 'http://www.google.com?q=dink',
+            onComplete : function(response) {
+                setTimeout(function(){
+                    console.log('Fake delay');
+                    console.log(response);
+                    searchBar.removeClass('loading disabled');
+                    youtubeResult.html(videos);
+                }, 3000);
+            },
+            onSuccess : function(response) {
+                console.log('Success');
+            },
+            onFailure : function(response) {
+                console.log('Failure');
+            }
+        })
 });
