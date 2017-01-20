27c44429c7679527da443b67318924a332afc8f7
Bart Willems <ps3bartwillems@gmail.com>
2017-01-21 00:37:23 +0100

    Disabled multiple api requests when holding enter

--- src/js/getYoutubeVideos.js
+++ src/js/getYoutubeVideos.js
@@ -2,22 +2,29 @@ $(function() {
 
     reloadCardHover();
 
-    var searchBar       = $('#searchBar');
+    var searchBar       = $('#searchBar');      // The div around the input dink
+    var inputBar        = $('#youtubeQuery');   // the input field itself
     var youtubeResult   = $('#youtubeResult');
     var videos          = youtubeResult.html();
+    var loading         = false;
 
     $('#youtubeQuery').keypress(function (e) {
-        if (e.which == 13) {
+        console.log(loading);
+        if (e.which == 13 && !loading) {
+            loading = true;
             $('form#search').submit();
             searchBar.addClass('loading disabled');
+            inputBar.prop('disabled', true);
+
             var loadingHTML  = '<div class="ui active inverted dimmer">';
-            loadingHTML += '<div class="ui text loader">Loading</div></div><p></p>';
+                loadingHTML += '<div class="ui text loader">Loading</div></div><p></p>';
+
             youtubeResult.html(loadingHTML);
             return false;
         }
     });
 
-    $('form#search')
+    $('.ui.form')
         .api({
             url: 'http://www.google.com?q=dink',
             onComplete : function(response) {
@@ -25,8 +32,10 @@ $(function() {
                     console.log('Fake delay');
                     console.log(response);
                     searchBar.removeClass('loading disabled');
+                    inputBar.prop('disabled', false);
                     youtubeResult.html(videos);
                     reloadCardHover();
+                    loading = false;
                 }, 3000);
             },
             onSuccess : function(response) {
