c8177f0fa0ff4998c56c42cd1532263bc5b34ef2
Bart Willems <ps3bartwillems@gmail.com>
2017-01-21 14:09:31 +0100

    Added fetch videos from backend coed
    Signed-off-by: Bart Willems <ps3bartwillems@gmail.com>

--- src/js/getYoutubeVideos.js
+++ src/js/getYoutubeVideos.js
@@ -7,6 +7,12 @@ $(function() {
     var youtubeResult   = $('#youtubeResult');
     var videos          = youtubeResult.html();
     var loading         = false;
+    var card            = null;
+
+    $.get('/templates/card.template.html', function(r) {
+        inputBar.prop('disabled', false);
+        card = r;
+    }, 'html');
 
     $.fn.api.settings.api = {
         'search' : '/api/search/?videos=1&query={query}'
@@ -20,7 +26,7 @@ $(function() {
             inputBar.prop('disabled', true);
 
             var loadingHTML  = '<div class="ui active inverted dimmer">';
-                loadingHTML += '<div class="ui text loader">Loading</div></div><p></p>';
+            loadingHTML += '<div class="ui text loader">Loading</div></div><p></p>';
 
             youtubeResult.html(loadingHTML);
             return false;
@@ -37,18 +43,25 @@ $(function() {
                 return settings;
             },
             onComplete : function(response) {
-                setTimeout(function(){
-                    console.log('Fake delay');
-                    console.log(response);
-                    searchBar.removeClass('loading disabled');
-                    inputBar.prop('disabled', false);
-                    youtubeResult.html(videos);
-                    reloadCardHover();
-                    loading = false;
-                }, 3000);
+                console.log(response);
+                searchBar.removeClass('loading disabled');
+                inputBar.prop('disabled', false);
+                loading = false;
             },
             onSuccess : function(response) {
                 console.log('Success');
+                var videoLength = response.results.length;
+                var videos      = response.results;
+                var html        = '';
+                for(var i = 0; i < videoLength; i++) {
+                    var video = videos[i];
+                    var videoCard = card;
+                    videoCard = videoCard.replace('{{thumbnail}}', video.snippet.thumbnails.high.url);
+                    videoCard = videoCard.replace('{{title}}', video.snippet.title)
+                    html = html + videoCard;
+                }
+                youtubeResult.html(html);
+                reloadCardHover();
             },
             onFailure : function(response) {
                 console.log('Failure');
@@ -57,7 +70,7 @@ $(function() {
 
     function reloadCardHover(){
         $('.special.cards .image').dimmer({
-              on: 'hover'
+            on: 'hover'
         });
     }
 });
