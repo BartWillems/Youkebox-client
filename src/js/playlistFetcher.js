c27bf091415162584b8c2bfd231327e68167d3d1
Bart Willems <ps3bartwillems@gmail.com>
2017-01-25 15:40:32 +0100

    Minor string fixes
    Signed-off-by: Bart Willems <ps3bartwillems@gmail.com>

--- src/js/playlistFetcher.js
+++ src/js/playlistFetcher.js
@@ -37,4 +37,14 @@ $(function() {
         }
         playlistBody.html(html);
     }
+
+    function getCurrentVideo() {
+        if($scope.playlist.length > 0) {
+            if($scope.video.data('id') !== $scope.playlist[0].video_id) {
+                // Set the currently playing video as the first entry in the playlist
+                $scope.video.data('id', $scope.playlist[0].video_id);
+            }
+        }
+        setTimeout(getCurrentVideo(), 1000);
+    }
 });
--- src/js/root.js
+++ src/js/root.js
@@ -8,17 +8,6 @@ $(function() {
         'search'    : 'api/search/?videos=1&query={query}',
         'add video' : 'api/add/video'
     };
-
-    function getCurrentVideo() {
-        if($scope.playlist.length > 0) {
-            if($scope.video.data('id') !== $scope.playlist[0].video_id) {
-                // Set the currently playing video as the first entry in the playlist
-                $scope.video.data('id', $scope.playlist[0].video_id);
-            }
-        }
-        setTimeout(getCurrentVideo(), 1000);
-    }
-
 });
 
 function parseDuration(duration) {
